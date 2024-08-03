043bc953fd4bc6dbbeb09314aff1fcab1ea183ca
Juiceee <lbehr@student.42.fr>
2024-08-03 15:26:46 +0200

    commencement

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,12 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <iostream>
+# include <string>
+
+template<typename T>
+T &easyfind(T &t, int nbr)
+{
+	
+}
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int p = 6;
+	int h = 48;
+
+	std::swap(p, h);
+
+	std::cout << p << h;
+}
